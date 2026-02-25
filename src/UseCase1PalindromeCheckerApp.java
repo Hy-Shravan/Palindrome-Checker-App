9e8c394c81fb533a8b3289d0595a458c90cb55b6
Hy-Shravan <shravan.per26@gmail.com>
2026-02-25 10:54:58 +0530

    Add welcome message and application details

--- src/UseCase1PalindromeCheckerApp.java
+++ src/UseCase1PalindromeCheckerApp.java
@@ -3,6 +3,7 @@ public class UseCase1PalindromeCheckerApp {
         // UC1: Application Entry & Welcome Message
         System.out.println("========================================");
         System.out.println("   Welcome to Palindrome Checker App    ");
+        
         System.out.println("========================================");
         System.out.println("Application Name: Palindrome Checker");
         System.out.println("Version: 1.0.0");
